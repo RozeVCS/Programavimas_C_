3944d2dc414107001748c91871f090efb22a364b
Hp <Hp@DESKTOP-2J2AOIE>
2023-10-16 15:06:55 +0300

    Add project files.

---
+++ Kintamieji_C#/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Kintamieji_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string studento_Vardas = "Jonas";
+            string studento_Pavarde = "Karalius";
+            var amzius = 33;
+            int studento_Amzius = 33;
+            float studento_Ugis = 175f;//centimetrai
+            int studento_Svoris = 90;
+            string aukstoji_Mokykla = "MRU";
+            int akademines_Grupes_Kodas = 76;
+            int kurso_Kodas = 121;
+            string studiju_Programos_Pav = "Viešasis administravimas";
+            int atsiskaitytu_Kreditu_Skaicius = 20;
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
